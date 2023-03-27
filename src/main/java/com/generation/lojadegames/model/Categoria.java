0137598f98f5b8f70220c38185cef7ec21ad8d73
Rafael <rafaelgouvea219@gmail.com>
2023-03-27 16:59:48 -0300

    Inclusão da camada security

---
+++ src/main/java/com/generation/lojadegames/model/Categoria.java
@@ -0,0 +1,65 @@
+package com.generation.lojadegames.model;
+
+import java.util.List;
+
+import org.hibernate.annotations.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;lidation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "A categoria é OBRIGATÓRIA!")
+	@Size(min = 5, max = 50, message = "A categoria deve ter no mínimo 05 e no máximo 50 caracteres")
+	private String categoria;
+	
+	@Size(min = 5, max = 200, message = "A descrição deve ter no mínimo 05 e no máximo 200 caracteres")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public List<Produto> getProduto() {
+		return produto;
+	}
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+	
+
+}
